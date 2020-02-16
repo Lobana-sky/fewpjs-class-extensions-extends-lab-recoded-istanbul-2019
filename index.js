a43fb3df22956a86eec9dd211b3d011345ec71e7
lobana alarabi <loubanaeng@gmail.com>
2020-02-16 13:41:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class Triangle extends Polygon{
   
   get isValid(){
     let sum=this.array[0]+this.array[1]
-    let sub=Math.abs(this.array[0]+this.array[1])
+    let sub=Math.abs(this.array[0]-this.array[1])
        if(this.countSides==3&&sum>this.array[2]&&sub<this.array[2]){
       return true;
     }
